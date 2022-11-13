ff8249c95756a297e7790f6cd8e7062e491d3d6e
Jasoonii <jason.ddang1@gmail.com>
2022-11-12 21:16:19 -0600

    9:16

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,26 +1,47 @@
+import { Icon, IconButton } from '@mui/material';
 import { NavLink } from 'react-router-dom';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faUser, faUserAstronaut, faMagnifyingGlass, faPlus, faHouse, faHatCowboy} from '@fortawesome/free-solid-svg-icons';
 
 import classes from './MainNavigation.module.css';
 
 const MainNavigation = () => {
   return (
     <header className={classes.header}>
-      <div className={classes.logo}><i class="fa-solid fa-book"></i> Reciply</div>
+      <NavLink to='/home' >
+      <div className={classes.logo}><i class="fa-solid fa-book"></i>
+        Reciply <FontAwesomeIcon icon ={faHatCowboy} />
+       </div>
+      </NavLink>
       <nav className={classes.nav}>
         <ul>
           <li>
             <NavLink to='/home' className={navData => navData.isActive ? classes.active : '' }>
-            Home
+              Home
+              <IconButton>
+                <FontAwesomeIcon icon={faHouse} />
+              </IconButton>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to='/search-page' className={navData => navData.isActive ? classes.active : '' }>
+              <IconButton>
+                <FontAwesomeIcon icon={faMagnifyingGlass} />
+              </IconButton>
             </NavLink>
           </li>
           <li>
             <NavLink to='/new-recipe' className={navData => navData.isActive ? classes.active : '' }>
-            Add a Recipe
+              <IconButton>
+                <FontAwesomeIcon icon={faPlus} />
+              </IconButton>
             </NavLink>
           </li>
           <li>
-            <NavLink to='/search-recipe' className={navData => navData.isActive ? classes.active : '' }>
-            Search
+            <NavLink to='/profile-page' className={navData => navData.isActive ? classes.active : '' }>
+              <IconButton>
+                <FontAwesomeIcon icon={faUserAstronaut} />            
+              </IconButton>
             </NavLink>
           </li>
         </ul>
