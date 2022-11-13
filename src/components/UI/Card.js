155a92eaecd362f2b9cea4e94bc9d733ee123281
Jasoonii <jason.ddang1@gmail.com>
2022-11-13 03:09:52 -0600

    3:10

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -1,3 +1,14 @@
-const Card = () => {}
+import classes from './Card.module.css'
+
+function Card(props) {
+    return (
+        <div className = {classes.card}>
+            <img src = {props.img} className = {classes.image} alt ="Food pic"/>
+            <h2 className = {classes.CardTitle}>{props.title}</h2>
+            <p className = {classes.Description}>{props.body}</p>
+            <button className = {classes.Button}>View Recipe!</button>
+        </div>
+    )
+}
 
 export default Card;
