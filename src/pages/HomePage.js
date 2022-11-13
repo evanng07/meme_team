bd15f4e30b79a3f02be4283ab7f9b42cefd93493
Jasoonii <jason.ddang1@gmail.com>
2022-11-13 03:09:48 -0600

    testCommit

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,10 +1,20 @@
-
+import Card from '../components/UI/Card'
 
 const HomePage = () => {
     return (
-        <div className="centered">
-            <p>HI GUYS</p>
-        </div>
+        <>
+            <div className="centered">
+                <p>Test</p>
+            </div>
+
+            <div className = "wrapper">
+                <Card 
+                    img = "https://images-gmi-pmc.edge-generalmills.com/1dc0506e-8ac3-49eb-b05c-04f413a0f101.jpg"
+                    body = "A super easy chicken pot pie recipe that will get your guests coming back!"
+                    title = "Chicken Pot Pie in 20 minutes!"
+                />
+            </div>
+        </>
     )
 }
 
