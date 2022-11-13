0d2caae5233414e70d29f038c7f2017f528a6d05
evanng07 <ngoeva072@gmail.com>
2022-11-12 22:07:51 -0600

    10:07

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,47 +1,64 @@
-import { Icon, IconButton } from '@mui/material';
-import { NavLink } from 'react-router-dom';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faUser, faUserAstronaut, faMagnifyingGlass, faPlus, faHouse, faHatCowboy} from '@fortawesome/free-solid-svg-icons';
+// import { IconButton } from "@mui/material";
+import { NavLink } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faUserAstronaut,
+  faMagnifyingGlass,
+  faPlus,
+  faHouse,
+  faHatCowboy,
+} from "@fortawesome/free-solid-svg-icons";
 
-import classes from './MainNavigation.module.css';
+import classes from "./MainNavigation.module.css";
 
 const MainNavigation = () => {
   return (
     <header className={classes.header}>
-      <NavLink to='/home' >
-      <div className={classes.logo}><i class="fa-solid fa-book"></i>
-        Reciply <FontAwesomeIcon icon ={faHatCowboy} />
-       </div>
+      <NavLink to="/home" className={classes.logo}>
+        <div className={classes.logo}>
+          <i class="fa-solid fa-book"></i>
+          Reciply <FontAwesomeIcon icon={faHatCowboy}/>
+        </div>
       </NavLink>
       <nav className={classes.nav}>
         <ul>
           <li>
-            <NavLink to='/home' className={navData => navData.isActive ? classes.active : '' }>
-              Home
-              <IconButton>
-                <FontAwesomeIcon icon={faHouse} />
-              </IconButton>
+            <NavLink
+              to="/search-page"
+              className={(navData) => (navData.isActive ? classes.active : "")}
+            >
+              <FontAwesomeIcon
+                icon={faMagnifyingGlass}
+                className={classes.icon}
+              />
             </NavLink>
           </li>
           <li>
-            <NavLink to='/search-page' className={navData => navData.isActive ? classes.active : '' }>
-              <IconButton>
-                <FontAwesomeIcon icon={faMagnifyingGlass} />
-              </IconButton>
+            <NavLink
+              to="/home"
+              className={(navData) => (navData.isActive ? classes.active : "")}
+            >
+              <FontAwesomeIcon icon={faHouse} className={classes.icon} />
+            </NavLink>
+          </li>
+
+          <li>
+            <NavLink
+              to="/new-recipe"
+              className={(navData) => (navData.isActive ? classes.active : "")}
+            >
+              <FontAwesomeIcon icon={faPlus} className={classes.wide} />
             </NavLink>
           </li>
           <li>
-            <NavLink to='/new-recipe' className={navData => navData.isActive ? classes.active : '' }>
-              <IconButton>
-                <FontAwesomeIcon icon={faPlus} />
-              </IconButton>
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to='/profile-page' className={navData => navData.isActive ? classes.active : '' }>
-              <IconButton>
-                <FontAwesomeIcon icon={faUserAstronaut} />            
-              </IconButton>
+            <NavLink
+              to="/profile-page"
+              className={(navData) => (navData.isActive ? classes.active : "")}
+            >
+              <FontAwesomeIcon
+                icon={faUserAstronaut}
+                className={classes.wide}
+              />
             </NavLink>
           </li>
         </ul>
